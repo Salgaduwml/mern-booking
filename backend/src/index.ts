943bbe8d49df9ee30cb76f7d59057ff564d26a8e
Malinda <salgaduwml@gmail.com>
2024-03-16 17:28:30 +0530

    Create user login api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 import userRoutes from "./routes/users.route";
+import authRoutes from "./routes/auth.route";
 
 mongoose.connect(process.env.MONGO_URI as string);
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server is listening!");
